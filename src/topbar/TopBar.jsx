55567acc5490893bf92ad2f66bb474d3e1e0b4d0
Andrew Chen <achen226@gmail.com>
2021-12-27 13:02:43 -0800

    put img in after a while cuz i have brain damage

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -1,11 +1,31 @@
 import "./topbar.css"
+import megumi from '../MegumiJinno_1.png'
 
 export default function TopBar() {
     return (
         <div className="top">
-            <div className="topLeft">l</div>
-            <div className="topCenter">c</div>
-            <div className="topRight">r</div>
+            <div className="topLeft">
+                <i className="topIcon fab fa-facebook-square"></i>
+                <i className="topIcon fab fa-twitter-square"></i>
+                <i className="topIcon fab fa-pinterest-square"></i>
+                <i className="topIcon fab fa-instagram-square"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">WRITE</li>
+                    <li className="topListItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="topRight">
+                <img className="topImg"
+                    src={megumi}
+                    alt=""
+                />
+                <i className="topSearchIcon fas fa-search"></i>
+            </div>
         </div>
     )
 }
