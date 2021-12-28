2e9be0ff022675199103a0bbf353abea2ed918ac
Andrew Chen <achen226@gmail.com>
2021-12-27 22:04:17 -0800

    settings page

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,9 +1,26 @@
 import './write.css'
+import megu_img from '../../assets/megu-img.jpeg'
 
 export default function Write() {
     return (
-        <div>
-            
+        <div className="write">
+            <img className="writeImg" src={megu_img} alt="" />
+            <form className="writeForm">
+                <div className="writeFormGroup">
+                    <label htmlFor="fileInput">
+                        <i class="writeIcon fas fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{display: "none"}}/>
+                    <input type="text" placeholder="Title" className="writeInput" autoFocus={true}/>
+
+                </div>
+                <div className="writeFormGroup">
+                    <textarea placeholder="Tell your story..." type="text" className="writeInput writeText">
+
+                    </textarea>
+                    <button className="writeSubmit">Publish</button>
+                </div>
+            </form>
         </div>
     )
 }
